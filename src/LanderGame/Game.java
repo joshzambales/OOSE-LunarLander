563b76fa9d20533e8a631925e62b20d6bab654fe
Frans Peter Larsen <larsen.frans@gmail.com>
2015-03-18 17:37:37 +0100

    Thrust rate goes down as you don't press W Clean Up

--- src/LanderGame/Game.java
+++ src/LanderGame/Game.java
@@ -1,9 +1,7 @@
 package LanderGame;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
 
 public class Game extends BasicGame {
 	private Player p;
@@ -15,9 +13,6 @@ public class Game extends BasicGame {
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		
-		g.drawString("Test", 250, 200);
 		p.render(gc,g);
 		
 		g.drawString("Rotation:" + p.getRotation(),10 , 30);
@@ -26,14 +21,11 @@ public class Game extends BasicGame {
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {
-		// TODO Auto-generated method stub
-		
-		p = new Player();
+	public void init(GameContainer gc) throws SlickException {		
+		p = new Player(new Vector2f(gc.getWidth()/2, gc.getHeight()/2));
 	}
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
-		// TODO Auto-generated method stub
 		p.update(gc,delta);
 	}
 }
--- src/LanderGame/Player.java
+++ src/LanderGame/Player.java
@@ -1,31 +1,48 @@
 package LanderGame;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.geom.*;
 
+/**
+ * The player spaceship which takes keyboard inputs, in which the user controls
+ * @author Frans Larsen
+ */
 public class Player {
-	public float THRUST_SPEED = 0.0025f;
+	public float THRUST_SPEED = 0.0025f; // Maximum allowed thrust speed
 	
 	private Vector2f position;		// x,y ships position in space
 	private Vector2f acceleration;	// x,y ships acceleration direction
 	private float rotation;			// current rotation of ship
-	private float thrust;
+	private float thrust;			// Force being added to the ships forward vector relative to its rotation
 	
-	Player() {
-		position = new Vector2f(1280/2,720/2);
-		acceleration = new Vector2f(0,0);
+	/**
+	 * Constructor for player, which handles the init of Position, acceleration, rotation & thrust
+	 * @param pos Position where the player should be initialised
+	 */
+	Player(Vector2f pos) {
+		position = pos;	
+		acceleration = new Vector2f(0,0);		
 		rotation = 0;
 		thrust = 0;
 	}
 	
-	void update(GameContainer gc, int delta) throws SlickException {
+	/**
+	 * Updates the players position, acceleration using gameinputs
+	 * @param gc GameContainer 
+	 * @param delta Delta frame time
+	 * @throws SlickException throws any SlickException
+	 */
+	public void update(GameContainer gc, int delta) throws SlickException {
 		Input key = gc.getInput();
 		
-		//TODO Take rotation into account rather than just moving with worldspace
 		if(key.isKeyDown(Input.KEY_W)) {
 			thrust += (THRUST_SPEED - thrust) * 0.0001;
-			acceleration.add(new Vector2f(thrust * (float)Math.cos(rotation),thrust * (float)Math.sin(rotation)));
+		} else {
+			thrust += (0 - thrust) * 0.01f;
 		}
+		
+		//TODO Add break using KEY_S
+		
 		if(key.isKeyDown(Input.KEY_A)) {
 			rotation -= Math.PI/640 * delta;
 		}
@@ -33,31 +50,55 @@ public class Player {
 			rotation += Math.PI/640 * delta;
 		}
 		
-		thrust += (0 - thrust) * 0.001f; // De-accelerate ship
+		//TODO Add de-acceleration from UNKNOWN FORCES FROM SPACE
 		
+		acceleration.add(new Vector2f(thrust * (float)Math.cos(rotation),thrust * (float)Math.sin(rotation)));
 		position.add(acceleration);
 	}
 	
-	void render(GameContainer gc, Graphics g) throws SlickException {
+	/**
+	 * Renders the ship for the player, including sprites, particles, etc.
+	 * @param gc GameContainer 
+	 * @param delta Delta frame time
+	 * @throws SlickException throws any SlickException
+	 */
+	public void render(GameContainer gc, Graphics g) throws SlickException {
 		//TODO Draw image and rotate image according to rotation vector
 		g.drawRect(position.x-5, position.y-5, 10, 10);
 		
 		g.drawLine(position.x, position.y, position.x+(float)(20*Math.cos(rotation)), position.y+(float)(20*Math.sin(rotation)));
 	}
 	
+	/**
+	 * Adds a force to the ships acceleration
+	 * @param v Is the vector of force to add
+	 */
 	public void addForce(Vector2f v) {
 		acceleration.add(v);
 	}
 	
+	/**
+	 * Returns the ships rotation in Radians
+	 * @return
+	 */
 	public float getRotation() {
 		return rotation;
 	}
 	
+	/**
+	 * Returns the ships thrust rate
+	 * @return
+	 */
 	public float getThrust() {
 		return thrust;
 	}
 	
+	/**
+	 * Returns the ships acceleration
+	 * @return
+	 */
 	public float getAcceleration() {
+		//FIXME I think this is wrong?
 		return (float) Math.sqrt(Math.pow(Math.cos(acceleration.x), 2) + Math.pow(Math.sin(acceleration.y), 2));
 	}
 }

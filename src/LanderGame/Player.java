30c60ce9eed2c178d98278ce8ff36731d517b171
Frans Peter Larsen <larsen.frans@gmail.com>
2015-03-19 10:09:15 +0100

    Debug line for current velocity Changed acceleration to velocity

--- src/LanderGame/Player.java
+++ src/LanderGame/Player.java
@@ -4,24 +4,24 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.geom.*;
 
 /**
- * The player spaceship which takes keyboard inputs, in which the user controls
+ * The player spaceship which moves around the world, in which the user controls.
  * @author Frans Larsen
  */
 public class Player {
-	public float THRUST_SPEED = 0.0025f; // Maximum allowed thrust speed
+	public float MAX_THRUST = 0.0025f; // Maximum allowed thrust speed (Not to be confused with velocity)
 	
 	private Vector2f position;		// x,y ships position in space
-	private Vector2f acceleration;	// x,y ships acceleration direction
+	private Vector2f velocity;	// x,y ships acceleration direction
 	private float rotation;			// current rotation of ship
 	private float thrust;			// Force being added to the ships forward vector relative to its rotation
 	
 	/**
-	 * Constructor for player, which handles the init of Position, acceleration, rotation & thrust
+	 * Constructor for player, which handles the initial of Position, acceleration, rotation & thrust
 	 * @param pos Position where the player should be initialised
 	 */
 	Player(Vector2f pos) {
-		position = pos;	
-		acceleration = new Vector2f(0,0);		
+		position = pos;
+		velocity = new Vector2f(0,0);		
 		rotation = 0;
 		thrust = 0;
 	}
@@ -36,7 +36,7 @@ public class Player {
 		Input key = gc.getInput();
 		
 		if(key.isKeyDown(Input.KEY_W)) {
-			thrust += (THRUST_SPEED - thrust) * 0.0001;
+			thrust += (MAX_THRUST - thrust) * 0.0001;
 		} else {
 			thrust += (0 - thrust) * 0.01f;
 		}
@@ -52,8 +52,8 @@ public class Player {
 		
 		//TODO Add de-acceleration from UNKNOWN FORCES FROM SPACE
 		
-		acceleration.add(new Vector2f(thrust * (float)Math.cos(rotation),thrust * (float)Math.sin(rotation)));
-		position.add(acceleration);
+		velocity.add(new Vector2f(thrust * (float)Math.cos(rotation),thrust * (float)Math.sin(rotation)));
+		position.add(velocity);
 	}
 	
 	/**
@@ -66,7 +66,12 @@ public class Player {
 		//TODO Draw image and rotate image according to rotation vector
 		g.drawRect(position.x-5, position.y-5, 10, 10);
 		
+		// Debugging
+		g.setColor(Color.red);	
+		g.drawLine(position.x, position.y, position.x + (500*velocity.x), position.y + (500*velocity.y));
+		g.setColor(Color.cyan);
 		g.drawLine(position.x, position.y, position.x+(float)(20*Math.cos(rotation)), position.y+(float)(20*Math.sin(rotation)));
+		g.setColor(Color.white);
 	}
 	
 	/**
@@ -74,7 +79,7 @@ public class Player {
 	 * @param v Is the vector of force to add
 	 */
 	public void addForce(Vector2f v) {
-		acceleration.add(v);
+		velocity.add(v);
 	}
 	
 	/**
@@ -99,6 +104,6 @@ public class Player {
 	 */
 	public float getAcceleration() {
 		//FIXME I think this is wrong?
-		return (float) Math.sqrt(Math.pow(Math.cos(acceleration.x), 2) + Math.pow(Math.sin(acceleration.y), 2));
+		return (float) Math.sqrt(Math.pow(Math.cos(velocity.x), 2) + Math.pow(Math.sin(velocity.y), 2));
 	}
 }

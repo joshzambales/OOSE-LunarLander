dd719f0b74f6cc5884b438acea6843f6641fc614
Jonastikov <jonastikov@gmail.com>
2015-03-20 14:56:30 +0100

    Gravity test done
    TODO collision

--- src/LanderGame/Game.java
+++ src/LanderGame/Game.java
@@ -15,7 +15,7 @@ public class Game extends BasicGame {
 	@Override
 	public void init(GameContainer gc) throws SlickException {		
 		player = new Player(new Vector2f(gc.getWidth()/2, gc.getHeight()/2));
-		planet = new Planet (new Vector2f(100, 400), 200, 10);
+		planet = new Planet (new Vector2f(100, 400), 200, 100.0f, 300.0f, 10);
 		
 		
 	}
@@ -28,10 +28,25 @@ public class Game extends BasicGame {
 		g.drawString("Rotation:" + player.getRotation() + " Rad",10 , 30);
 		g.drawString("Velocity: " + player.getVelocity() + " pix/ms", 10, 60);
 		g.drawString("Thrust: " + player.getThrust() + " pix/ms", 10, 90);
+		g.drawString("Distance: " + distance(), 10, 120);
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 		player.update(gc,delta);
+
+		if (distance() <= planet.getGravityRange()){
+			planet.color = Color.yellow;
+		}
 	}
+
+	public float distance() {
+		double x, y;
+		x = Math.pow((planet.getPos().x - player.getPosition().x), 2);
+		y = Math.pow((planet.getPos().y - player.getPosition().y), 2);
+		return (float) Math.sqrt(x+y);
+	}
+	
+	
+	
 }
--- src/LanderGame/Planet.java
+++ src/LanderGame/Planet.java
@@ -12,16 +12,28 @@ public class Planet {
 
 	// Properties
 	private Vector2f location;		// x,y planets location in space
-	private int size;					// Size of planet
+	private float gravity;				// Amount of gravity
+	private float gravityRange;			// Reach of gravity
+	private float size;					// Size of planet
 	private int resource; 				// Amout of resources
+
 	
 	// Resources
 	private Image texPlanet;			// Image/texture for Planet
 
-	Planet(Vector2f pos, int size, int resource){
+	// Debug
+	public Color color = Color.green;
+	
+	/**
+	 * Constructor for planet, which handles the initial of Position of a planet
+	 * @param pos Position where the planet should be initialised
+	 */
+	Planet(Vector2f pos, int size, float gravity, float gravityRange, int resource){
 		location = pos;
 		this.size = size;
 		this.resource = resource;	
+		this.gravity = gravity;
+		this.gravityRange = gravityRange + size;
 	}
 	
 	
@@ -29,32 +41,56 @@ public class Planet {
 		
 	}
 	
-	/**
-	 * Constructor for planet, which handles the initial of Position of a planet
-	 * @param pos Position where the planet should be initialised
-	 */
-	public void getPlanetPos(Vector2f pos){
-		location = pos;
+	
+	
+	public Vector2f getPos(){
+		return location;
 	}
 	
-	public void resPlanet(int res){
+	public int getPlanetRes(){
+		return resource;
+	}
+	
+	public void setPlanetRes(int res){
+		if(res < 0){
+			res = 0;
+		}
 		resource = res;
 	}
 	
-	
+
 	
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		// debugging 
 		
+		g.drawRect(location.x-5, location.y-5, 10, 10);
+		g.setColor(color);
 		g.drawOval(location.x-(size/2), location.y-(size/2), size, size);
+		g.setColor(Color.magenta);
+		g.drawOval(location.x-((gravityRange)/2), location.y-((gravityRange)/2), gravityRange, gravityRange);
 		g.setColor(Color.white);
+
 
 	}
 	
+	
+	// TODO do the math!
 	public void gravity(){
-		
+		gravity = this.size * 0.01f;
 		
 	}
+
+
+	public float getGravityRange() {
+		return gravityRange/2;
+	}
+
+
+	public void setGravityRange(float gravityRange) {
+		this.gravityRange = gravityRange;
+	}
+
+
 	
 //	public boolean hasLanded(Vector2f playerPos){
 //		boolean bool = false;
